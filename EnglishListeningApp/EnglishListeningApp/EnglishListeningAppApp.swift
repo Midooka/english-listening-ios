56ff7fd008079cdf479f3db5980a2c05246fd43f
Midooka <t-a-midooka@mercari.com>
2026-02-07 22:24:24 +0900

    chore: create Xcode SwiftUI app project

---
+++ EnglishListeningApp/EnglishListeningApp/EnglishListeningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishListeningAppApp.swift
+//  EnglishListeningApp
+//
+//  Created by t-a-midooka on 2026/02/07.
+//
+
+import SwiftUI
+
+@main
+struct EnglishListeningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
